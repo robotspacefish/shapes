8679bbdec89efb4089962c399f6fc01f0aeff177
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-11-12 23:24:31 -0500

    Shape base class

---
+++ shapes/Shape.cpp
@@ -0,0 +1,17 @@
+#include "Shape.hpp"
+
+namespace ShapeAssessment
+{
+//    Shape::Shape(float value1, float value2) : mValue1(value1), mValue2(value2)
+//    {
+//    }
+    
+    void Shape::setValue1(float v) {
+        mValue1 = v;
+    }
+
+    void Shape::setValue2(float v) {
+        mValue2 = v;
+    }
+
+}
---
+++ shapes/Shape.hpp
@@ -0,0 +1,13 @@
+//
+//  Shape.hpp
+//  shapes
+//
+//  Created by Jess on 11/12/20.
+//
+
+#ifndef Shape_hpp
+#define Shape_hpp
+
+#include <stdio.h>
+
+#endif /* Shape_hpp */
