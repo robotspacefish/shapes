c7126795a8f6c867d3ea8fafa6ec191a21042ee7
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-11-12 21:32:42 -0500

    Initial Commit

---
+++ shapes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  shapes
+//
+//  Created by Jess on 11/12/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
