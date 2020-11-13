27dea9332c1d7d7f972b4df21348549503c4f784
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-11-12 23:27:50 -0500

    inherit from Shape

--- shapes/Rectangle.cpp
+++ shapes/Rectangle.cpp
@@ -1,13 +1,16 @@
 #include "Rectangle.hpp"
 
+
 namespace ShapeAssessment
 {
-    Rectangle::Rectangle(float length, float width) : mLength(length), mWidth(width)
+    Rectangle::Rectangle(float length, float width)
     {
+        this->setValue1(length);
+        this->setValue2(width);
     }
 
     float Rectangle::getArea()
     {
-        return mLength * mWidth;
+        return mValue1 * mValue2;
     }
 }
--- shapes/Rectangle.hpp
+++ shapes/Rectangle.hpp
@@ -1,15 +1,14 @@
 #pragma once
 
+#include "Shape.hpp"
+
 namespace ShapeAssessment
 {
-    class Rectangle
+    class Rectangle : public Shape
     {
     public:
         Rectangle(float length, float width);
         
         float getArea();
-    private:
-        float mLength;
-        float mWidth;
     };
 }
